d938a73c17c0f377ab79e4d64f02a89e556e161d
saiteja623 <saiteja.balla13@gmail.com>
2020-08-12 14:26:36 +0530

    Updates

---
+++ precache-manifest.e6f651266f9ef0212a7ef106f33794f5.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f02ff014e974cbe87d400f05c1b04a89",
+    "url": "/instaclone/index.html"
+  },
+  {
+    "revision": "8cf74bfa07f9a2154fa5",
+    "url": "/instaclone/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "69b8e695ec4995ef3912",
+    "url": "/instaclone/static/css/main.205ce932.chunk.css"
+  },
+  {
+    "revision": "8cf74bfa07f9a2154fa5",
+    "url": "/instaclone/static/js/2.f6f49ea4.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/instaclone/static/js/2.f6f49ea4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69b8e695ec4995ef3912",
+    "url": "/instaclone/static/js/main.0e115c4e.chunk.js"
+  },
+  {
+    "revision": "625834bd94f88f1bcc85",
+    "url": "/instaclone/static/js/runtime-main.b8d28f71.js"
+  },
+  {
+    "revision": "52b04f1c2bd73f240b4ad620924a40c9",
+    "url": "/instaclone/static/media/Billabong.52b04f1c.ttf"
+  },
+  {
+    "revision": "358bec321e3192e48e8de22cc09e284d",
+    "url": "/instaclone/static/media/bg.358bec32.png"
+  },
+  {
+    "revision": "01cfa6472c719acdcc7d2b199087c4da",
+    "url": "/instaclone/static/media/comment.01cfa647.png"
+  },
+  {
+    "revision": "bf6582eafe98be385c1767924162ddd3",
+    "url": "/instaclone/static/media/empty.bf6582ea.png"
+  },
+  {
+    "revision": "f4e6aca4007067625ba50ed02a492192",
+    "url": "/instaclone/static/media/friend-request.f4e6aca4.png"
+  },
+  {
+    "revision": "d4d4dc3f55ea581423e05ab82a7d411d",
+    "url": "/instaclone/static/media/post.d4d4dc3f.png"
+  }
+]);
